10f47ff5a24a42234b32b3201a50af423feebbf3
Yuliya Daroshka <yuliya.daroshka@gmail.com>
2019-06-20 21:58:55 +0000

    Done.

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,2 +1,12 @@
 # Don't forget! This file needs to be 'required' in its spec file
 # See README.md for instructions on how to do this
+
+def fizzbuzz(num)
+  if num % 3 == 0 && num % 5 == 0
+    puts "FizzBuzz"
+  elsif num % 3 == 0
+    puts "Fizz"
+  else num % 5 == 0
+    puts "Buzz"
+  end
+end
